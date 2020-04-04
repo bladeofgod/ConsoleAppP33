e5bde805d07f27622ff69885e15645c4d136d067
bladeofgod <450103187@qq.com>
2020-04-04 19:20:16 +0800

    p73

--- ConsoleAppP33.cpp
+++ ConsoleAppP33.cpp
@@ -81,8 +81,47 @@ void Clock::setTime(int h,int m,int s) {
     second = s;
 }
 
+
+///p73
+enum CPU_Rank {P1=1,P2,P3,P4,P5,P6,P7};
+
+
+class Cpu {
+private:
+    CPU_Rank rank;
+    int frequency;
+    float voltage;
+public:
+    Cpu(CPU_Rank r, int f, float v) {
+        rank = r;
+        frequency = f;
+        voltage = v;
+        cout << "init constructor function" << endl;
+    }
+    ~Cpu() { cout << "restore a cpu" << endl; }
+
+    CPU_Rank getRank()const { return rank; }
+    int getFrequency()const { return frequency; }
+    float getVoltage()const { return voltage; }
+
+    void setRank(CPU_Rank r) { rank = r; }
+    void setFrequency(int f) { frequency = f; }
+    void setVoltage(float v) { voltage = v; }
+
+    void run() { cout << "run cpu"<<endl; }
+    void stop() {cout<<"stop cpu"<<endl}
+
+};
+
+
 int main()
 {   
+
+    ///p73
+    Cpu a(P6,300,2.8);
+    a.run;
+    a.stop;
+
 
     ///p60
 
